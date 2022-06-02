5d93f0759c168314a2a7b86875b859f4c53fa7c6
Dima Glebov <78321515+CommanderCoolDev@users.noreply.github.com>
2022-06-02 13:47:46 +0300

    added hooks

--- src/App.js
+++ src/App.js
@@ -1,58 +1,79 @@
-import React from "react";
-
+import React from 'react';
 
 class App extends React.Component {
-    state = {
-        count: 0,
-        isCounting: false,
-    };
-  handleStart = () => {this.timerId = setInterval(() => {
-    this.setState((prevState) => ({ count: prevState.count + 1, isCounting: true }),)
-  }, 1000)}
-  
+  state = {
+    count: 0,
+    isCounting: false,
+  };
+  handleStart = () => {
+    this.timerId = setInterval(() => {
+      this.setState(prevState => ({
+        count: prevState.count + 1,
+        isCounting: true,
+      }));
+    }, 1000);
+  };
+
   handleStop = () => {
     clearInterval(this.timerId);
-    this.setState({isCounting: false})
-  }
+    this.setState({ isCounting: false });
+  };
   handleReset = () => {
     this.setState({ count: 0, isCounting: false });
-     clearInterval(this.timerId);
- 
-  }
+    clearInterval(this.timerId);
+  };
   componentDidMount() {
-    let data = localStorage.getItem('count')
-  
+    let data = localStorage.getItem('count');
+
     if (data) {
-       this.setState({ count: +data })
-       console.log(data)
+      this.setState({ count: +data });
+      console.log(data);
     }
-   
-    
   }
-  
 
   componentDidUpdate() {
-    localStorage.setItem('count', `${this.state.count}`)
-      
-    }
+    localStorage.setItem('count', `${this.state.count}`);
+  }
 
   componentWillUnmount() {
     clearInterval(this.timerId);
-    
-    }
+  }
 
-    render() {
-        return (
-            <div className="App">
-                <h1>React Timer</h1>
-                <h3>{this.state.count}</h3>
-                {!this.state.isCounting ? (
-                    <button onClick={this.handleStart}>Start</button>
-                ) : (
-                    <button onClick={this.handleStop}>Stop</button>
-                )}
-                <button onClick={this.handleReset}>Reset</button>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="App">
+        <h1>React Timer</h1>
+        <h3>{this.state.count}</h3>
+        {!this.state.isCounting ? (
+          <button onClick={this.handleStart}>Start</button>
+        ) : (
+          <button onClick={this.handleStop}>Stop</button>
+        )}
+        <button onClick={this.handleReset}>Reset</button>
+      </div>
+    );
+  }
 }
+//or it may be on hooks
+// import React, { useState } from 'react';
+
+// function App() {
+//   const [count, setCount] = useState(0);
+//   const increment = () => {
+//     setCount(count + 1);
+//   };
+
+//   const decrement = () => {
+//     setCount(count - 1);
+//   };
+
+//   return (
+//     <div className="App">
+//       <button onClick={decrement}>-</button>
+//       <span>{count}</span>
+//       <button onClick={increment}>+</button>
+//     </div>
+//   );
+// }
+
+// export default App;
